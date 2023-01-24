b0b7e3a2401ac4b985f05b97c0cdb38ea76c098a
kim_ingyeom <doctok501@gmail.com>
2023-01-24 10:07:28 -0800

    dev-JPA

--- jpashop/src/main/java/jpabook/jpashop/web/MemberForm.java
+++ jpashop/src/main/java/jpabook/jpashop/web/MemberForm.java
@@ -1,13 +1,17 @@
 package jpabook.jpashop.web;
 
+import jakarta.annotation.Nonnull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.lang.annotation.Documented;
 
 
 @Getter @Setter
 public class MemberForm {
 
     //Not empty annotation 찾아보기
+    @Nonnull
     private String name;
     private String city;
     private String street;
