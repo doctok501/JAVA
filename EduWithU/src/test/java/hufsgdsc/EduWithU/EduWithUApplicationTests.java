2d473b9b7373a86f3faf7b52411053a3d2241571
kim_ingyeom <doctok501@gmail.com>
2023-01-29 14:07:28 -0700

    eduwithu

---
+++ EduWithU/src/test/java/hufsgdsc/EduWithU/EduWithUApplicationTests.java
@@ -0,0 +1,14 @@
+package hufsgdsc.EduWithU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduWithUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	
+}
